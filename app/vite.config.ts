7d15ce26cd6dcc7bce297e5069e45a64ea2a6e8c
Angelet-code <sagara.ad@gmail.com>
2026-06-04 10:23:13 +0200

    Deploy app to GitHub Pages

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Left2Eat-v2/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
